251365a8fffeb74e084535bdbc2f7160b60658d3
kt <kntkb.qbr@gmail.com>
2024-02-07 19:10:19 -0500

    update test_app_sampler.py

--- espfit/tests/test_app_sampler.py
+++ espfit/tests/test_app_sampler.py
@@ -8,7 +8,7 @@ ESPALOMA_FORCEFIELD = str(files('espfit').joinpath('data/forcefield/espaloma-0.3
 
 
 @pytest.fixture
-def test_create_test_espaloma_system():
+def test_create_test_espaloma_system(tmpdir):
     """Fixture function to create a test system.
 
     Returns
@@ -16,13 +16,13 @@ def test_create_test_espaloma_system():
     c : espfit.app.sampler.SetupSampler
     """
     biopolymer_file = files('espfit').joinpath('data/target/testsystems/nucleoside/pdbfixer_min.pdb')   # PosixPath
-    c = SetupSampler(small_molecule_forcefield=ESPALOMA_FORCEFIELD)
-    c.create_system(biopolymer_file=biopolymer_file)
+    c = SetupSampler(small_molecule_forcefield=ESPALOMA_FORCEFIELD, output_directory_path=str(tmpdir))
+    c.create_system(biopolymer_file=biopolymer_file)  # Exports solvated system as pdb file automatically.
 
     return c
 
 
-def test_create_nucleoside_espaloma_system():
+def test_create_nucleoside_espaloma_system(tmpdir):
     """Test creating nucleoside system using espaloma forcefield.
     
     All solute atoms will be parameterized using the espaloma forcefield.
@@ -32,11 +32,11 @@ def test_create_nucleoside_espaloma_system():
     None
     """
     biopolymer_file = files('espfit').joinpath('data/target/testsystems/nucleoside/pdbfixer_min.pdb')
-    c = SetupSampler(small_molecule_forcefield=ESPALOMA_FORCEFIELD)
+    c = SetupSampler(small_molecule_forcefield=ESPALOMA_FORCEFIELD, output_directory_path=str(tmpdir))
     c.create_system(biopolymer_file=biopolymer_file)
 
 
-def test_create_protein_ligand_espaloma_system():
+def test_create_protein_ligand_espaloma_system(tmpdir):
     """Test creating protein-ligand system using espaloma forcefield.
     
     All solute atoms will be parameterized using the espaloma forcefield.
@@ -48,11 +48,11 @@ def test_create_protein_ligand_espaloma_system():
     biopolymer_file = files('espfit').joinpath('data/target/testsystems/protein-ligand/target.pdb')
     # Convert PosixPath to str to avoid error when loading molecule with RDKIT.
     ligand_file = str(files('espfit').joinpath('data/target/testsystems/protein-ligand/ligands.sdf'))
-    c = SetupSampler(small_molecule_forcefield=ESPALOMA_FORCEFIELD)
+    c = SetupSampler(small_molecule_forcefield=ESPALOMA_FORCEFIELD, output_directory_path=str(tmpdir))
     c.create_system(biopolymer_file=biopolymer_file, ligand_file=ligand_file)
 
 
-def test_create_protein_ligand_openff_system():
+def test_create_protein_ligand_openff_system(tmpdir):
     """Test creating protein-ligand system using openff forcefield.
     
     Protein and ligand parameterized with Amber ff14SB and openff-2.1.0, respectively.
@@ -64,12 +64,12 @@ def test_create_protein_ligand_openff_system():
     biopolymer_file = files('espfit').joinpath('data/target/testsystems/protein-ligand/target.pdb')
     # Convert PosixPath to str to avoid error when loading molecule with RDKIT.
     ligand_file = str(files('espfit').joinpath('data/target/testsystems/protein-ligand/ligands.sdf'))
-    c = SetupSampler(small_molecule_forcefield='openff-2.1.0')
+    c = SetupSampler(small_molecule_forcefield='openff-2.1.0', output_directory_path=str(tmpdir))
     c.create_system(biopolymer_file=biopolymer_file, ligand_file=ligand_file)
 
 
 @pytest.mark.skip("Time demanding test. Skip to speedup the test suite.")
-def test_create_protein_espalom_system():
+def test_create_protein_espalom_system(tmpdir):
     """Test creating protein system using espaloma force field.
     
     All solute atoms will be parameterized using the espaloma forcefield.
@@ -79,12 +79,12 @@ def test_create_protein_espalom_system():
     None
     """
     biopolymer_file = files('espfit').joinpath('data/target/testsystems/protein-ligand/target.pdb')
-    c = SetupSampler(small_molecule_forcefield=ESPALOMA_FORCEFIELD)
+    c = SetupSampler(small_molecule_forcefield=ESPALOMA_FORCEFIELD, output_directory_path=str(tmpdir))
     c.create_system(biopolymer_file=biopolymer_file)
 
 
 @pytest.mark.skip("Time demanding test. Skip to speedup the test suite.")
-def test_create_multi_protein_ligand_espaloma_system():
+def test_create_multi_protein_ligand_espaloma_system(tmpdir):
     """Test creating multi-protein and ligand system using espaloma forcefield.
     
     All solute atoms will be parameterized using the espaloma forcefield.
@@ -96,7 +96,7 @@ def test_create_multi_protein_ligand_espaloma_system():
     biopolymer_file = files('espfit').joinpath('data/target/testsystems/multi_protein-ligand/target.pdb')
     # Convert PosixPath to str to avoid error when loading molecule with RDKIT.
     ligand_file = str(files('espfit').joinpath('data/target/testsystems/multi_protein-ligand/ligands.sdf'))
-    c = SetupSampler(small_molecule_forcefield=ESPALOMA_FORCEFIELD)
+    c = SetupSampler(small_molecule_forcefield=ESPALOMA_FORCEFIELD, output_directory_path=str(tmpdir))
     c.create_system(biopolymer_file=biopolymer_file, ligand_file=ligand_file)
 
 
@@ -131,14 +131,13 @@ def test_export_system_change_outdir(test_create_test_espaloma_system, tmpdir):
 
     tmpdir : tmpdir fixture from pytest
 
-
     Returns
     -------
     None
     """
     c = test_create_test_espaloma_system
     old_outdir = c.output_directory_path
-    c.export_xml(output_directory_path=str(tmpdir))
+    c.export_xml(output_directory_path=str(tmpdir.join('newdir')))
     new_outdir = c.output_directory_path
     
     assert old_outdir != new_outdir
@@ -174,6 +173,7 @@ def test_standard_md(test_create_test_espaloma_system, tmpdir):
     """
     c = test_create_test_espaloma_system
     c.output_directory_path = str(tmpdir)
+    c.minimize(maxIterations=10)  # Minimize the system before running the simulation to avoid Energy NaN.
     c.run(nsteps=10)
 
 
