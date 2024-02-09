ef0420006ee673bbddc11ba70418ef33ae46f7d9
kt <kntkb.qbr@gmail.com>
2024-02-09 15:25:57 -0500

    use pint.UnitRegistry to convert units

--- espfit/utils/units.py
+++ espfit/utils/units.py
@@ -1,3 +1,13 @@
-# Constants for unit conversions
-HARTEE_TO_KCALPERMOL = 627.509
-BOHR_TO_ANGSTROMS = 0.529177
+from pint import UnitRegistry
+
+# Define pint unit registry
+ureg = UnitRegistry()
+hartree = 1 * ureg.hartree
+bohr = 1 * ureg.bohr
+angstrom = 1 * ureg.angstrom
+
+# Conversion factors
+#HARTEE_TO_KCALPERMOL = 627.509
+#BOHR_TO_ANGSTROMS = 0.529177
+HARTREE_TO_KCALMOL = hartree.to(ureg.kilocalorie/(ureg.avogadro_constant*ureg.mole)).magnitude
+BOHR_TO_ANGSTROMS = bohr.to(ureg.angstrom).magnitude
