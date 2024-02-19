628440fe4fdc77cf597aae287e2d1ec4afc0fbcb
kt <kntkb.qbr@gmail.com>
2024-02-19 15:58:43 -0500

    convert unit string into openmm unit

--- espfit/utils/units.py
+++ espfit/utils/units.py
@@ -1,3 +1,4 @@
+import openmm.unit as unit
 from pint import UnitRegistry
 
 # Define pint unit registry
@@ -11,3 +12,41 @@ angstrom = 1 * ureg.angstrom
 #BOHR_TO_ANGSTROMS = 0.529
 HARTREE_TO_KCALPERMOL = hartree.to(ureg.kilocalorie/(ureg.avogadro_constant*ureg.mole)).magnitude
 BOHR_TO_ANGSTROMS = bohr.to(ureg.angstrom).magnitude
+
+
+def convert_string_to_unit(unit_string):
+    """Convert a unit string to a openmm unit object.
+    
+    Parameters
+    ----------
+    unit_string : str
+        The string representation of the unit.
+
+    Returns
+    -------
+    openmm.unit
+        The openmm unit object.
+    """
+    unit_mapping = {
+        "nanometer": unit.nanometer,
+        "angstrom": unit.angstrom,
+        "nanometers": unit.nanometers,
+        "angstroms": unit.angstroms,
+        "kelvin": unit.kelvin,
+        "molar": unit.molar,
+        "millimolar": unit.millimolar,
+        "micromolar": unit.micromolar,
+        "atomsphere": unit.atmosphere,
+        "bar": unit.bar,
+        "nanoseconds": unit.nanoseconds,
+        "picoseconds": unit.picoseconds,
+        "femtoseconds": unit.femtoseconds,
+        "nanosecond": unit.nanosecond,
+        "picosecond": unit.picosecond,
+        "femtosecond": unit.femtosecond,
+        # Add more units as needed
+    }
+    if unit_string in unit_mapping:
+        return unit_mapping[unit_string]
+    else:
+        raise ValueError(f"Unit '{unit_string}' is not recognized.")
