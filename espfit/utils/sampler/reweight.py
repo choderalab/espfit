bdb143384a5ff46ae6c4593e00f6dc4b19e3b7da
kt <kntkb.qbr@gmail.com>
2024-02-28 13:10:08 -0500

    add support to compute sampler loss

--- espfit/utils/sampler/reweight.py
+++ espfit/utils/sampler/reweight.py
@@ -5,16 +5,28 @@ from espfit.app.sampler import SetupSampler, BaseSimulation
 _logger = logging.getLogger(__name__)
 
 
-class SamplerReweight(SetupSampler, BaseSimulation):
+class SetupSamplerReweight(SetupSampler, BaseSimulation):
 
     def __init__(self, weight=1, **kwargs):
         super().__init__(**kwargs)
         self.weight = weight
 
+        
+    def _get_experiment_data(self, target_class, target_name):
+        import yaml
+        from importlib.resources import files
+
+        yaml_file = str(files('espfit').joinpath(f'data/target/{target_class}/{target_name}/experiment.yml'))
+        with open(yaml_file, 'r') as f:
+            d = yaml.safe_load(f)
+
+        # {'resi_1': {'1H5P': {'name': 'beta_1', 'value': None, 'operator': None, 'error': None}}}
+        return d['experiment_1']['measurement']
+
 
     def get_effective_sample_size(self):
         # Compute effective sample size
-        neff = 0.5
+        neff = 0.5        
         return neff
 
 
@@ -23,25 +35,53 @@ class SamplerReweight(SetupSampler, BaseSimulation):
             from espfit.app.analysis import RNASystem
             target = RNASystem()
             target.load_traj(input_directory_path=self.output_directory_path)
-            val = target.compute_jcouplings()
-            _logger.info(f'Computed observable: {val}')
+            pred = target.compute_jcouplings()
+            #_logger.debug(f'Computed observable: {pred}')
         else:
             raise NotImplementedError(f'Observable for {self.target_class} is not implemented.')
 
         import yaml
         with open(os.path.join(self.output_directory_path, 'pred.yaml'), 'w') as f:
-            yaml.dump(val, f, allow_unicode=True)
+            yaml.dump(pred, f, allow_unicode=True)
 
-        return val
+        return pred
 
 
     def compute_loss(self):
         # Compute experimental observable
-        val = self._compute_observable()
-        _logger.info(f'Compute loss')
+        exp = self._get_experiment_data(self.target_class, self.target_name)
+        pred = self._compute_observable()
+
+        loss = []
+        for resi_index, exp_dict in enumerate(exp.values()):
+            for key, value in exp_dict.items():
+                # {'1H5P': {'name': 'beta_1', 'value': None, 'operator': None, 'error': None}}
+                if value['operator'] in ['>', '<', '>=', '<=', '~'] or value['value'] == None:
+                    # Dont use uncertain data
+                    pass
+                else:
+                    exp_value = value['value']
+                    exp_error = value['error']
+                    if exp_error == None:
+                        exp_error = 0.5  # TODO: Check experimental error
+
+                    resi_index = int(resi_index)
+                    pred_value = list(pred.values())[resi_index][key]['avg']
+                    pred_error = list(pred.values())[resi_index][key]['std']  # standard deviation
+
+                    # TODO: change to debug
+                    _logger.info(f'Exp ({resi_index}-{key}): {exp}')
+                    _logger.info(f'Pred ({resi_index}-{key}): {pred}')
+
+                    # Compute loss
+                    numerator = (pred_value - exp_value) ** 2
+                    dominator = (exp_error ** 2 + pred_error ** 2)
+                    loss.append(numerator / dominator)
+                
 
         # Compute loss
         import torch
-        loss = torch.tensor(0.0)
+        loss_avg = torch.mean(torch.tensor(loss))
+        _logger.info(f'Computed sampler loss: {loss_avg.item()}')
 
-        return loss
+        return loss_avg
